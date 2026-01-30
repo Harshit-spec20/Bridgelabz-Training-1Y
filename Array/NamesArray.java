abac5fcd884a0097c2a08c2e5947730555f05032
harshitsingh876 <dynamon876@gmail.com>
2026-01-30 10:00:53 +0530

    Names

---
+++ Array/NamesArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class NamesArray{
+	public static void main(String[]args) {
+		
+		String[] names={"Harshit","Prince","Saksham","Karnav","Yug"};
+		 System.out.println("List of names:");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+	}
+}
